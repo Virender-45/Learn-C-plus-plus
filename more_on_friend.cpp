74fb6de162eccd806eec5382a8761fe0e99077cd
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-12 08:47:42 +0530

    Create more_on_friend.cpp

---
+++ more_on_friend.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Y;
+
+class X{
+    int data;
+    friend void add(X, Y);
+    public:
+        void setValue(int value){
+            data = value;
+        }
+};
+class Y{
+    int num;
+    friend void add(X, Y);
+    public:
+        void setValue(int value){
+            num = value;
+        }
+};
+
+void add(X o1, Y o2){
+    cout<<"Summing datas of X and Y objects gives me : "<<o1.data + o2.num<<endl; 
+}
+
+int main(){
+    X a;
+    Y b;
+
+    a.setValue(10);
+    b.setValue(20);
+
+    add(a, b);
+
+    return 0;
+}
