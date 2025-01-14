180e855158e51e650f6475d36fbd68a08169f2bd
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-15 02:00:27 +0530

    Update inheritance.cpp

--- inheritence/inheritance.cpp
+++ inheritence/inheritance.cpp
@@ -1,14 +1,46 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-//DRY - Don't Repeat Yourself
-class {};
 
-int main() {
-    
+//Base Class
+class Employee{
+    //Private member never inherits
+    public:
+    int id; 
+        float salary;
+        Employee(int inpId){
+            id = inpId;
+            salary = 34.0;
+        }
+        Employee(){}
+};
+
+//Derived Class //Default visibility mode is private
+class Programmer : Employee{
+    public:
+        int languageCode;
+        Programmer(int inpId){
+            id = inpId;
+            languageCode = 9;
+        }
+        void display(){
+            cout<<id<<endl;
+        }
+};
+
+
+int main(){
+    Employee virender(1), rohan(2);
+    cout << virender.salary << endl;
+    cout << rohan.salary << endl;
+
+    Programmer skillf(2);
+    cout<<skillf.languageCode<<endl;
+    cout<<skillf.id<<endl;
 
     return 0;
 }
 
+//DRY - Don't Repeat Yourself
 /*
   Single Inheritance
   Multiple Inheritance    One base multiple derrived classes
