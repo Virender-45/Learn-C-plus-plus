7e3b9d18faaff94454812eae6eefe45a074cb03b
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-15 01:03:48 +0530

    Create inheritance.cpp

---
+++ inheritence/inheritance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+//DRY - Don't Repeat Yourself
+class {};
+
+int main() {
+    
+
+    return 0;
+}
+
+/*
+  Single Inheritance
+  Multiple Inheritance    One base multiple derrived classes
+  hierarchical Inheritance    Several derived class from more than one base class
+  Multilevel Interitance    Deriving a class from already derived class
+  Hybrid Inheritance    It it a combination of multiple and multilevel inheritance
+
+*/
