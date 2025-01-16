bfc974d6604e611ce05c8eb16fa751f6517f9e65
Virender Rana <45virendersingh@gmail.com>
2025-01-16 21:48:41 +0530

    Format

--- inheritence/single_inheritence.cpp
+++ inheritence/single_inheritence.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -49,8 +49,10 @@ int main() {
 
 	return 0;
 }*/
-class Base {
+class Base
+{
 	int data1;
+
 public:
 	int data2;
 	void setData();
@@ -58,37 +60,45 @@ public:
 	int getData2();
 };
 
-void Base::setData() {
+void Base::setData()
+{
 	data1 = 10;
 	data2 = 20;
 }
 
-int Base::getData1() {
+int Base::getData1()
+{
 	return data1;
 }
-int Base::getData2() {
+int Base::getData2()
+{
 	return data2;
 }
 
-class Derived: private Base{		//With private
+class Derived : private Base
+{ // With private
 	int data3;
+
 public:
 	void process();
 	void display();
 };
 
-void Derived::process() {
+void Derived::process()
+{
 	setData();
 	data3 = data2 * getData1();
 }
 
-void Derived::display() {
+void Derived::display()
+{
 	cout << "Value of data 1 is : " << getData1() << endl;
 	cout << "Value of data 2 is : " << data2 << endl;
 	cout << "Value of data 3 is : " << data3 << endl;
 }
 
-int main() {
+int main()
+{
 	Derived obj;
 	// obj.setData();	Not accessible
 	obj.process();
