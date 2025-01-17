5549657daacc008736c550a74b8a19ceda4c2fbd
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-18 01:02:08 +0530

    Update excercise.cpp

--- inheritance/excercise.cpp
+++ inheritance/excercise.cpp
@@ -7,14 +7,55 @@ class simpleCalculator{
         int a, b;
     public:
         void setNumbers(int, int);
-        
+        void displaySimpleMenu();
+        int add(int, int);
+        int sub(int, int);
+        int div(int, int);
+        int mult(int, int);
 };
 void simpleCalculator::setNumbers(int num1, int num2){
         a = num1;
         b = num2;
 }
-
-class scientificCalculator{};
+void simpleCalculator::displaySimpleMenu(){
+    cout<<"Welcome to Simple calculator"<<endl;
+    cout<<"1. Addition"<<endl;
+    cout<<"2. Subtraction"<<endl;
+    cout<<"3. Division"<<endl;
+    cout<<"4. Multiplication"<<endl;
+    cout<<"Enter your choice : ";
+}
+int simpleCalculator::add(int num1, int num2){
+    return num1 + num2;
+}
+int simpleCalculator::sub(int num1, int num2){
+    return num1 - num2;
+}
+int simpleCalculator::div(int num1, int num2){
+    if (num2 != 0){
+        return num1 / num2;
+    }
+    else{
+        cout << "Error: Division by zero is not allowed." << endl;
+    }
+}
+int simpleCalculator::mult(int num1, int num2){
+    return num1 * num2;
+}
+class scientificCalculator{
+    protected:
+        float a,b;
+int z;
+clrscr();
+void Power(float,float);
+void Sine(float);
+void Square(float);
+void Cos(float);
+void Tan(float);
+void Log(float);
+void Baselog(float);
+        
+};
 
 int main(){
     
