0c8c256ce4bec6fb788fba43537146dbbdab9c06
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-18 02:00:58 +0530

    Update excercise.cpp

--- inheritance/excercise.cpp
+++ inheritance/excercise.cpp
@@ -57,44 +57,46 @@ public:
     void Baselog(float);
 };
 void scientificCalculator::displayScientificMenu(){
-    cout<<""
+    cout<<"Welcome to scientificCalculator"<<endl;
+    cout<<"1. Power"<<endl;
+    cout<<"2. Sine"<<endl;
+    cout<<"3. Square"<<endl;
+    cout<<"4. Cosine"<<endl;
+    cout<<"5. Tangent"<<endl;
+    cout<<"6. Natural Logarithm"<<endl;
+    cout<<"7. Base 10 Logarithm"<<endl;
+    cout<<"Enter your choice : ";
 }
 void scientificCalculator::Power(float x,float y){
 float p;
 p = pow(x,y);
 cout<<"Power: "<<p;
 }
-
 void scientificCalculator::Sine(float x){
 float s;
 s = sin(x);
 cout<<"Sin: "<<s;
 }
-
 void scientificCalculator::Square(float x){
 float sq;
 sq = sqrt(x);
 cout<<"Square of a Given Value is: "<<sq;
 }
-
 void scientificCalculator::Cos(float x){
 float c;
 c = cos(x);
 cout<<"COS: "<<c;
 }
-
 void scientificCalculator::Tan(float x){
 float t;
 t = tan(x);
 cout<<"TAN: "<<t;
 }
-
 void scientificCalculator::Log(float x){
 float l;
 l = log(x);
 cout<<"Natural Logarithm: "<<l;
 }
-
 void scientificCalculator::Baselog(float x){
 float bl;
 bl = log10(x);
@@ -102,10 +104,86 @@ cout<<"LOG with Base 10: "<<bl;
 }
 
 class hybridCalculator: public simpleCalculator, public scientificCalculator{
-    
+    protected:
+        int choice;
+        float num1, num2;
+    public:
+        void displayHybridMenu();
+
 };
+void hybridCalculator::displayHybridMenu(){
+    cout<<"Welcome to Hybrid Calculator"<<endl;
+    cout<<"1. Simple Calculator"<<endl;
+    cout<<"2. Scientific Calculator"<<endl;
+    cout<<"Enter your choice : ";
+}
 
 int main(){
+    hybridCalculator calc;
+    calc.displayHybridMenu();
+    int choice;
+    cin>>choice;
+    if(choice==1){
+        calc.displaySimpleMenu();
+        int choice;
+        cin>>choice;
+        float num1, num2;
+        cout<<"Enter two numbers: ";
+        cin>>num1>>num2;
+        calc.setNumbers(num1, num2);
+        switch(choice){
+            case 1:
+                cout<<"Addition of given numbers is : "<<calc.add(num1, num2)<<endl;
+                break;
+            case 2:
+                cout<<"Subtraction of given numbers is : "<<calc.sub(num1, num2)<<endl;
+                break;
+            case 3:
+                cout<<"Division of given numbers is : "<<calc.div(num1, num2)<<endl;
+                break;
+            case 4:
+                cout<<"Multiplication of given numbers is : "<<calc.mult(num1, num2)<<endl;
+                break;
+            default:
+                cout<<"Invalid choice. Please try again."<<endl;
+        }
+    }
+    else if(choice==2){
+        calc.displayScientificMenu();
+        int choice;
+        cin>>choice;
+        float num1;
+        cout<<"Enter a number: ";
+        cin>>num1;
+        switch(choice){
+            case 1:
+                calc.Power(num1, 2);
+                break;
+            case 2:
+                calc.Sine(num1);
+                break;
+            case 3:
+                calc.Square(num1);
+                break;
+            case 4:
+                calc.Cos(num1);
+                break;
+            case 5:
+                calc.Tan(num1);
+                break;
+            case 6:
+                calc.Log(num1);
+                break;
+            case 7:
+                calc.Baselog(num1);
+                break;
+            default:
+                cout<<"Invalid choice. Please try again."<<endl;
+        }
+    }
+    else{
+        cout<<"Invalid choice. Please try again."<<endl;
+    }
 
     return 0;
 }
