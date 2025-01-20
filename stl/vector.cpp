65e43ddc4092a6972bc9a0d314314e123b727a48
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-21 01:05:40 +0530

    Update vector.cpp

--- stl/vector.cpp
+++ stl/vector.cpp
@@ -4,7 +4,20 @@
 using namespace std;
 
 int main(){
-  
+    vector<int> vec = {1, 2, 3, 4, 5};
 
+    //Print with foreach loop
+    for(int i : vec){
+        cout<< i <<" ";
+    }
+
+    //Print with for loop
+    //vector<int>::iterator i; can be write as auto
+    for(auto i = vec.begin(); i != vec.end(); i++){
+        cout<< *(i) <<" ";
+    }
+    for(auto i = vec.rbegin(); i != vec.rend(); i++){
+        cout<< *(i) <<" ";
+    }
   return 0;
 }
