ea1f7ccf1f274369b9e0f075b54869816f2ca241
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-21 01:23:05 +0530

    Update lists.cpp

--- stl/lists.cpp
+++ stl/lists.cpp
@@ -4,6 +4,18 @@
 using namespace std;
 
 int main(){
-    
+    list<int> l = {1, 2, 3, 4, 5};
+
+    // l.push_back(3);
+    // l.push_back(4);
+    // l.push_back(5);
+    // l.push_front(2);
+    // l.push_front(1);
+
+    for(int val : l){
+        cout<< val <<" ";
+    }
+    cout<< endl;
+
   return 0;
 }
