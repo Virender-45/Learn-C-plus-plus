caea6fca38c40964218807447c509a7af8242ac6
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-20 16:51:34 +0530

    Develope ambiguity

--- inheritance/ambiguity.cpp
+++ inheritance/ambiguity.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+class Base1{
+  public:
+    void greet(){
+      cout<<"How are you"<<endl;
+    }
+};
+
+class Base2{
+  public:
+    void greet(){
+      cout<<"Kaise Ho"<<endl;
+    }
+};
+
+class Derived : public Base1, public Base2{
+  
+};
+
 int main(){
 
   return 0;
