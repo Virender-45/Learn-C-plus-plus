4ca664713b5e879500b08258ab5f94489408ec14
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-22 01:16:46 +0530

    Update deque.cpp

--- stl/deque.cpp
+++ stl/deque.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    deque<int> d = {1, 2, 3, 4, 5};   //Inititialize as dynamic array in memory
+    deque<int> d = {1, 2, 3, 4, 5};   //Inititialize as dynamic array in memory , random access is possible
     //Same functions as list
 
     for( int val : d){
