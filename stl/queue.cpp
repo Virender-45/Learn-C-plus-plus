d648710b9a862649849b8967722d576763800144
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-23 00:50:08 +0530

    Add priority queue

--- stl/queue.cpp
+++ stl/queue.cpp
@@ -20,8 +20,22 @@ int main(){
         cout<<q.front() << " ";
         q.pop();
     }
+    cout<< endl;
 
     //Priority Queue
+
+    priority_queue<int> pq;     //Place the highest value at the top of the que
+
+    pq.push(1);
+    pq.push(23);
+    pq.push(435);
+    pq.push(45);
+    pq.push(3);
+
+    while(!pq.empty()){
+        cout<<pq.top()<< endl;
+        pq.pop();   //start popping from top
+    }
 
 
     return 0;
