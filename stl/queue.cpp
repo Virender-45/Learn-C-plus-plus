bd012b775b2eea18e3e70c734ec838348c9dfc7c
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-23 01:12:42 +0530

    Add priority queue
    Add priority queue with reverse order

--- stl/queue.cpp
+++ stl/queue.cpp
@@ -24,7 +24,7 @@ int main(){
 
     //Priority Queue
 
-    priority_queue<int> pq;     //Place the highest value at the top of the que
+    priority_queue<int> pq;     //Place the highest value at the top of the queue
 
     pq.push(1);
     pq.push(23);
@@ -32,9 +32,23 @@ int main(){
     pq.push(45);
     pq.push(3);
 
+    priority_queue<int, vector<int>, greater<int>> pqr;    //If we want reverse order priority queue, means lowest value remains on top
+    pqr.push(1);
+    pqr.push(23);
+    pqr.push(435);
+    pqr.push(45);
+    pqr.push(3);
+
     while(!pq.empty()){
         cout<<pq.top()<< endl;
         pq.pop();   //start popping from top
+    }
+
+    cout<< endl;
+
+    while(!pqr.empty()){
+        cout<<pqr.top()<< endl;
+        pqr.pop();   //start popping from top
     }
 
 
