436e97f60e5ddccac44eb3602669cc4d98f228c8
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-23 02:22:35 +0530

    Create set.cpp
    Created set , multi set and unordered set

---
+++ stl/set.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <deque>
+#include <stack>
+#include <thread>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set<int> s; // Doesnot store duplicate values        O(logn)
+
+    s.insert(1);
+    s.insert(3);
+    s.insert(4);
+    s.insert(2);
+    s.insert(2);
+    s.insert(5);
+    s.insert(2);
+    s.insert(2);
+
+    cout << "Lower bound is : " << *(s.lower_bound(4)) << endl;
+    cout << "Upper bound is : " << *(s.upper_bound(4)) << endl;
+
+    for (auto p : s)
+    {
+        cout << p << " ";
+    }
+    cout << "\n"
+         << s.size();
+
+    //Multi Set         //can store duplicate values
+
+
+
+    //unordered Set     //Very optimized        //Doesnot sort the values, values stores in unsroted way
+    //Lower and upper bound doesnot exist in unorderedd sets
+
+    return 0;
+}
