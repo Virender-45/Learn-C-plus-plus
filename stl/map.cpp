fff45065e36b7e5b6575f72667c2ea4469800c61
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-23 01:43:40 +0530

    Add multimap

--- stl/map.cpp
+++ stl/map.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main(){
     //(key, value) Key is always unique
-    map<string, int> m;
+    /*map<string, int> m;
 
     m["Tv"] = 200;
     m["Laptop"] = 150;
@@ -24,7 +24,6 @@ int main(){
     // m.erase("Tv");
 
 
-
     for(auto p : m){
         cout<<p.first<<" "<< p.second<<endl;    //data comes in ascending order and sorted order
     }
@@ -36,6 +35,26 @@ int main(){
     }
     else{
         cout<<"Not found"<<endl;
+    }
+    */
+
+    //Multimap
+
+    multimap<string, int> mm;   //Can insert multiple vales with same key
+
+    mm.emplace("tv", 100);
+    mm.emplace("tv", 453);
+    mm.emplace("tv", 10450);
+    mm.emplace("tv", 13500);
+    // mm.emplace("camera", 345);
+    // mm.emplace("headphone", 678);
+    // mm.emplace("laptop", 234);
+
+    // mm.erase("tv");  //Erase all keys started with tv
+    // mm.erase(mm.find("tv"));    //Erase only first key
+
+    for(auto p : mm){
+        cout<< p.first<<" "<< p.second<<endl;
     }
 
     return 0;
