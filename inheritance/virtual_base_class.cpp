4939824a1dc23af413f8b5787e349aff927d3fd2
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-24 01:13:17 +0530

    Update virtual_base_class.cpp
    Understand with example

--- inheritance/virtual_base_class.cpp
+++ inheritance/virtual_base_class.cpp
@@ -1,28 +1,72 @@
-#include <iostream> 
-using namespace std; 
+#include <iostream>
 
-class A { 
-public: 
-    int a; 
-    A() // constructor 
-    { 
-        a = 10; 
-    } 
-}; 
+using namespace std;
 
-class B : public virtual A { 
-}; 
+class Student{
+protected:
+    int roll_no;
 
-class C : public virtual A { 
-}; 
+public:
+    void set_number(int a){
+        roll_no = a;
+    }
+    void print_number(){
+        cout << "Your roll no is : " << roll_no << endl;
+    }
+};
 
-class D : public B, public C { 
-}; 
+class Test : virtual public Student{
+protected:
+    float maths, physics;
 
-int main() 
-{ 
-    D object; // object creation of class d 
-    cout << "a = " << object.a << endl; 
+public:
+    void set_marks(float m1, float m2){
+        maths = m1;
+        physics = m2;
+    }
+    void print_marks(){
+        cout<<"Your marks in "<< endl
+            <<"Maths is : "<< maths << endl
+            <<"Physics is : "<< physics << endl;
+    }
+};
 
-    return 0; 
+class Sports : virtual public Student{
+    protected:
+        float score;
+    public:
+        void set_score(float sc){
+            score = sc;
+        }
+        void print_score(){
+            cout<<"Your sport score is : "<< score <<endl;
+        }
+};
+
+class Result : public Test, public Sports{
+    private:
+        float total, avg;
+    public:
+        void display(){
+            total = maths + physics + score;
+            avg = (total/3);
+            print_number();
+            print_marks();
+            print_score();
+            cout<<"Total score is : "<< total <<endl;
+            cout<<"Average is : "<< avg << "%" << endl;
+        }
+};
+
+int main(){
+
+    Result vir;
+
+    vir.set_number(10);
+    vir.set_marks(10, 45);
+    vir.set_score(56);
+    vir.display();
+
+
+    return 0;
 }
