26744b5ad9cee2a297ef4cfb5df47ac08f6e9f08
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-24 22:13:00 +0530

    Update binary-search.cpp

--- stl/Algorithms/Time Complexity/binary-search.cpp
+++ stl/Algorithms/Time Complexity/binary-search.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
-#include <algorithm>>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int binarySearch(int arr[], int s, int e, int target){
-    while(s <= e){
-        int mid = s + (e - s)/2;
-        if(arr[mid] < target){
+int binarySearch(int arr[], int s, int e, int target)
+{
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (arr[mid] < target)
+        {
             s = mid + 1;
         }
-        else if(arr[mid] > target){
+        else if (arr[mid] > target)
+        {
             e = mid - 1;
         }
-        else{
+        else
+        {
             return mid;
         }
     }
 }
 
-int main(){
+
+
+int main()
+{
     int arr[10] = {3, 5, 7, 1, 9, 8, 2, 4, 6, 10};
+    vector<int> vec = {3, 5, 7, 1, 9, 8, 2, 4, 6, 10};
 
-    sort(arr, arr + 10);
-
-    for(int val : arr){
-        cout<<val <<" ";
+    for (int val : arr)
+    {
+        cout << val << " ";
     }
 
-   cout<< binarySearch(arr, 0, 9, 4);
+    cout << "\n"
+         << binarySearch(arr, 0, 9, 4) << endl;
+    cout << binary_search(vec.begin(), vec.end(), 4); // returns bool
+    
 
     return 0;
 }
