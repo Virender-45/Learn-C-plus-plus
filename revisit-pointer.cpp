8441eead5df682b4d702e09c148b22c2b48b0082
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-27 20:50:27 +0530

    Update revisit-pointer.cpp

--- revisit-pointer.cpp
+++ revisit-pointer.cpp
@@ -10,9 +10,22 @@ int main()
     int *ptr = &a;
     cout << *(ptr) << endl;
 
+    // new keyword
     int *p = new int(40);
-
     cout << *(p) << endl;
+
+    int* arr = new int[3];
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    *(arr + 3) = 40;
+    cout<<"arr[0] = "<< arr[0]<<endl;
+    cout<<"arr[1] = "<< arr[1]<<endl;
+    cout<<"arr[2] = "<< arr[2]<<endl;
+    cout<<"arr[3] = "<< arr[3]<<endl;
+
+    //Delete operator
+    delete [] arr;
 
     return 0;
 }
