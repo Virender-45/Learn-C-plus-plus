37a9491a0e44bbf5c411fe49a4e0f02786120183
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-28 03:10:26 +0530

    Update array-of-objects.cpp

--- array-of-objects.cpp
+++ array-of-objects.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 using namespace std;
 
-class ShopItem
-{
+class ShopItem{
     int id;
     float price;
 
 public:
-    void setData(int a, float b)
-    {
+    void setData(int a, float b){
         id = a;
         price = b;
     }
-    void getData(void)
-    {
+    void getData(void){
         cout << "The id of item is : " << id << endl;
         cout << "The price of item is : " << price << endl;
     }
 };
 
-int main()
-{
+int main(){
     int p, i;
     float q;
     int size = 2;
@@ -29,8 +25,7 @@ int main()
     ShopItem *ptr = new ShopItem[size];
     ShopItem *ptrTemp = ptr;
 
-    for (i = 0; i < size; i++)
-    {
+    for (i = 0; i < size; i++){
         cout << "Enter the id of item " << i + 1 << " : ";
         cin >> p;
         cout << "Enter the price of item " << i + 1 << " : ";
@@ -39,8 +34,7 @@ int main()
         ptr->setData(p, q);
         ptr++;
     }
-    for (i = 0; i < size; i++)
-    {
+    for (i = 0; i < size; i++){
         cout << "The details of item " << i + 1 << " is : " << endl;
         ptrTemp->getData();
         ptrTemp++;
