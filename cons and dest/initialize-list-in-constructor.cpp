1826a22b2d8c37d360e9f18f915f6612817bd6cd
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-27 19:58:57 +0530

    Update initialize-list-in-constructor.cpp

--- cons and dest/initialize-list-in-constructor.cpp
+++ cons and dest/initialize-list-in-constructor.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
-#include <stdint.h>
 
 using namespace std;
 
-/*
+class Test{
+    int a, b;
+    public:
+        Test(int i, int j): a(i), b(j){
+            cout<<"Constructor is called "<<endl;
+            cout<<"Value of a is : " << a <<endl;
+            cout<<"Value of b is : " << b <<endl;
+        }
+};
 
-
-*/
 int main(){
+    Test obj(4, 7);    
 
     return 0;
 }
