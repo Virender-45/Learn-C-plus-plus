32bb1f33605b07d10dbf153eca466172be83541d
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-28 02:18:37 +0530

    Create pointers-to-object.cpp

---
+++ pointers-to-object.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Complex{
+    int a, b;
+
+public:
+    void setData(int r, int i){
+        a = r;
+        b = i;
+    }
+    void display(){
+        cout << "The real number is : " << a << endl;
+        cout << "The imaginary number is : " << b << endl;
+    }
+};
+
+int main(){
+    Complex c1;
+    // Complex *ptr = &c1;
+    Complex *ptr = new Complex;
+
+    // c1.setData(1, 2);
+    // c1.dispplay();
+
+    // (*ptr).setData(11, 22);
+    // (*ptr).display();
+
+    ptr->setData(1, 4);
+    ptr->display();
+
+    return 0;
+}
