eaf19a9fb6c5f73eec63f774c2d4d9749c0f28e2
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-29 01:03:47 +0530

    Create polymorphism.cpp

---
+++ polymorphism.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    
+
+
+    return 0;
+}
+/*
+Polymorphism:
+1)Compile time polymorphism.
+    acheived using function, operator overloading.
+2)Run time polymorphism.
+    acheived using virtual functions.
+*/
