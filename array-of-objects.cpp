c083a31063f01cefc80e45ded4960da3f7b7a85f
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-28 20:08:07 +0530

    Added an example

--- array-of-objects.cpp
+++ array-of-objects.cpp
@@ -42,3 +42,72 @@ int main(){
 
     return 0;
 }
+
+
+/*
+            Example
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shop {
+    string product;
+    int pId, stock;
+    float price;
+
+public:
+    // Shop(string p, int id, int s, float pr) : product(p), pId(id), stock(s), price(pr){}
+
+    void setData(string p, int id, int s, float pr) {
+        product = p;
+        pId = id;
+        price = pr;
+        stock = s;
+    }
+    void getData(void) {
+        cout << "The product name is : " << product << endl;
+        cout << "The product id is : " << pId << endl;
+        cout << "The product has " << stock << " left in stock " << endl;
+        cout << "The price of product is : " << price << endl
+            << endl;
+    }
+};
+
+int main()
+{
+    int size = 2;
+    Shop* ptr = new Shop[size];
+    Shop* ptrTemp = ptr;
+    string p;
+    float price;
+    int id, stock;
+
+    for (int i = 0; i < size; i++) {
+        cout << "Enter the details of product " << i + 1 << endl;
+        cout << "Enter Name : ";
+        getline(cin, p);
+        cout << "Enter ID : ";
+        cin >> id;
+        cout << "Enter stock : ";
+        cin >> stock;
+        cout << "Enter price : ";
+        cin >> price;
+        cout << endl;
+
+		cin.ignore(); // to ignore the buffer
+
+        ptr->setData(p, id, stock, price);
+
+        ptr++;
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << "The detail of product " << i + 1 << " is" << endl;
+        ptrTemp->getData();
+        ptrTemp++;
+    }
+
+    return 0;
+}
