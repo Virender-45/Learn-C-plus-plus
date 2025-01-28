607bfeaf1fe62e55c498c9c3b73341e56e13c37e
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-29 02:16:19 +0530

    Update pointers to derived class.cpp

--- pointers to derived class.cpp
+++ pointers to derived class.cpp
@@ -1,36 +1,39 @@
 #include <iostream>
 using namespace std;
 
-class Base{
+class Base
+{
 public:
     int var_base;
-    void display(){
+    void display()
+    {
         cout << "Displaying base class var : " << var_base << endl;
     }
 };
 
-class Derived : public Base{
+class Derived : public Base
+{
 public:
     int var_derived;
-    void display(){
+    void display()
+    {
         cout << "Displaying base class var : " << var_base << endl;
         cout << "Displaying derived class var : " << var_derived << endl;
     }
 };
 
-int main(){
-    Base* base_ptr;
+int main()
+{
+    Base *base_ptr;
     Base base_obj;
     Derived derived_obj;
     base_ptr = &derived_obj;
 
     base_ptr->var_base = 10;
-    base_ptr->display();        //Even it is pointing to derived class it will invoke the base class functions, this is the rule
-    
-    Derived * derived_ptr;
-    derived_ptr = &derived_obj;     //can access both classes members
+    base_ptr->display(); // Even it is pointing to derived class it will invoke the base class functions, this is the rule
 
-
+    Derived *derived_ptr;
+    derived_ptr = &derived_obj; // can access both classes members
 
     return 0;
 }
