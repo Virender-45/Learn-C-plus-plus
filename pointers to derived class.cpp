2420facf13a5d040df112fe6ca818ebf01e754f2
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-29 02:15:11 +0530

    Update pointers to derived class.cpp

--- pointers to derived class.cpp
+++ pointers to derived class.cpp
@@ -20,16 +20,17 @@ public:
 
 int main(){
     Base* base_ptr;
-
     Base base_obj;
-
     Derived derived_obj;
-
     base_ptr = &derived_obj;
 
     base_ptr->var_base = 10;
     base_ptr->display();        //Even it is pointing to derived class it will invoke the base class functions, this is the rule
     
+    Derived * derived_ptr;
+    derived_ptr = &derived_obj;     //can access both classes members
+
+
 
     return 0;
 }
