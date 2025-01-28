9483a0878ee6000e92ba33b67dc313cfc4e11568
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-29 02:08:15 +0530

    Create pointers to derived class.cpp

---
+++ pointers to derived class.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Base{
+public:
+    int var_base;
+    void display(){
+        cout << "Displaying base class var : " << var_base << endl;
+    }
+};
+
+class Derived : public Base{
+public:
+    int var_derived;
+    void display(){
+        cout << "Displaying base class var : " << var_base << endl;
+        cout << "Displaying derived class var : " << var_derived << endl;
+    }
+};
+
+int main(){
+    Base* base_ptr;
+
+    Base base_obj;
+
+    Derived derived_obj;
+
+    base_ptr = &derived_obj;
+
+    base_ptr->var_base = 10;
+    base_ptr->display();        //Even it is pointing to derived class it will invoke the base class functions, this is the rule
+    
+
+    return 0;
+}
