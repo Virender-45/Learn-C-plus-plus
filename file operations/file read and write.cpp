5f7025e110ae1229e1d18019360463ebd74e323a
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-31 01:46:53 +0530

    Create file read and write.cpp

---
+++ file operations/file read and write.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+
+int main(){
+    string str = "Virender Singh" , str2;
+
+    //opeing file using constructor and writing it
+    ofstream out("sample.txt");
+    out<<str;
+
+    //opening file using constructor and reading it
+    ifstream in("sample2.txt");
+    // in>>str2;    //stop reading after spaces 
+    getline(in, str2);
+    cout<<str2;
+
+
+    return 0;
+}
