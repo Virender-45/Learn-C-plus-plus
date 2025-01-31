b39f1ec7c5bc6e2cc1275c8c8be5ead0f828110d
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-02-01 00:38:40 +0530

    Create open and eof.cpp

---
+++ file operations/open and eof.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ofstream out;
+    out.open("name.txt");
+    out << "This is Virender" << endl;
+    out << "This is Sahil" << endl;
+    out << "This is Aman" << endl;
+    out << "This is Abhay" << endl;
+    out.close();
+
+    ifstream in;
+    in.open("name.txt");
+    string str;
+    //in >> str;
+    //getline(in, str);
+
+    while (in.eof() == 0) {  //eof = End of file
+        getline(in, str);
+        cout << str << endl;
+    }
+    in.close();
+
+
+    return 0;
+}
