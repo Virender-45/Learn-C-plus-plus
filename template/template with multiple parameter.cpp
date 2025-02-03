55e21410891502713a90ec5d7aea305b4fc738a8
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-02-03 11:36:58 +0530

    Create template with multiple parameter.cpp

---
+++ template/template with multiple parameter.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+template<class T1, class T2>
+class myClass{
+        T1 data1;
+        T2 data2;
+    public:
+
+        myClass(T1 a, T2 b): data1(a), data2(b){}
+
+        void display(){
+            cout<<this->data1 << " "<< this->data2<<endl;
+        }
+};
+
+int main(){
+    myClass<int, char> obj(1, 'v');
+    obj.display();
+
+    myClass<int, string> obj2(2, "Virender");
+    obj2.display();
+
+    return 0;
+}
