1da110b7fb177e6ebc289024fd62b3ef2136e2ea
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-02-05 21:09:43 +0530

    Create function-objects.cpp

---
+++ stl/function-objects.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    // Function Objects(functor): function wrapped in a class so that it available like an object
+    int arr[] = {4, 8, 2, 32, 1, 6};
+    int n = 6;
+
+    sort(arr, arr+n, greater<int>());
+
+    for (int val : arr)
+    {
+        cout << val << endl;
+    }
+
+    return 0;
+}
