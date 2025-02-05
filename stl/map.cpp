8ab9e81d84cf79259251cabcf62fb590b68b0873
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-02-05 20:49:19 +0530

    Update map.cpp

--- stl/map.cpp
+++ stl/map.cpp
@@ -1,15 +1,12 @@
-#include<iostream>
+#include <iostream>
 #include <map>
 #include <unordered_map>
 
 using namespace std;
 
-
-int main(){
-    
-
-
-    //Map is an associative array
+int main()
+{
+    // Map is an associative array
     //(key, value) Key is always unique
     /*map<string, int> m;       //Store data in order way
 
@@ -39,41 +36,55 @@ int main(){
     }
     */
 
-    //Multimap
-/*
-    multimap<string, int> mm;   //Can insert multiple vales with same key
+    // Multimap
+    /*
+        multimap<string, int> mm;   //Can insert multiple vales with same key
 
-    mm.emplace("tv", 100);
-    mm.emplace("tv", 453);
-    mm.emplace("tv", 10450);
-    mm.emplace("tv", 13500);
-    // mm.emplace("camera", 345);
-    // mm.emplace("headphone", 678);
-    // mm.emplace("laptop", 234);
+        mm.emplace("tv", 100);
+        mm.emplace("tv", 453);
+        mm.emplace("tv", 10450);
+        mm.emplace("tv", 13500);
+        // mm.emplace("camera", 345);
+        // mm.emplace("headphone", 678);
+        // mm.emplace("laptop", 234);
 
-    // mm.erase("tv");  //Erase all keys started with tv
-    // mm.erase(mm.find("tv"));    //Erase only first key
+        // mm.erase("tv");  //Erase all keys started with tv
+        // mm.erase(mm.find("tv"));    //Erase only first key
 
-    for(auto p : mm){
-        cout<< p.first<<" "<< p.second<<endl;
+        for(auto p : mm){
+            cout<< p.first<<" "<< p.second<<endl;
+        }
+        */
+
+    /*
+    //unordered_map
+
+    unordered_map<string, int> um;   //Store data into unorder way
+
+     um["Tv"] = 200;
+     um["Laptop"] = 150;
+     um["Headphone"] = 400;
+     um["Phone"] = 300;
+     um["Watch"] = 350;
+
+     for(auto p : um){
+         cout<< p.first << " "<<p.second<<endl;
+     }
+     */
+
+    map<string, int> marksMap;
+
+    marksMap["Virender"] = 88;
+    marksMap["Rana"] = 34;
+    marksMap["Sahil"] = 63;
+
+    marksMap.insert({"Aman", 78});
+    marksMap.emplace("Abhay", 65);
+    map<string, int>::iterator iter;
+    for (iter = marksMap.begin(); iter != marksMap.end(); iter++)
+    {
+        cout << (*iter).first << " " << (*iter).second << endl;
     }
-    */
-
-   /*
-   //unordered_map
-
-   unordered_map<string, int> um;   //Store data into unorder way
-
-    um["Tv"] = 200;
-    um["Laptop"] = 150;
-    um["Headphone"] = 400;
-    um["Phone"] = 300;
-    um["Watch"] = 350;
-
-    for(auto p : um){
-        cout<< p.first << " "<<p.second<<endl;
-    }
-    */
 
     return 0;
 }
