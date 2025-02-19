3f48a8c2676440e6e2a80774060c4ff00407e572
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-02-19 21:07:26 +0530

    Update reverse.cpp

--- stl/Algorithms/reverse.cpp
+++ stl/Algorithms/reverse.cpp
@@ -16,6 +16,7 @@ using namespace std;
 int main()
 {
     vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int> vec2 = {10, 20, 30, 40, 50, 60, 70, 80, 90};
 
     reverse(vec.begin(), vec.end());
     // reverse(vec.begin() + 3, vec.begin() + 7);        //For a specific range
