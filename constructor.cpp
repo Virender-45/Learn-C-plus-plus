56a4dbc9ec303f8bc1163ad644ced1814ecba3ba
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-02-21 19:38:46 +0530

    Update constructor.cpp

--- constructor.cpp
+++ constructor.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class Complex{
+class Complex
+{
     int a,b;
     public:
         //Creating a constructor, it is a special member function tith same name as class
