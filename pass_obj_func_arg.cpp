82a4ea34e8fc24309d115007cd10429acae1e310
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-02-27 16:51:21 +0530

    Update pass_obj_func_arg.cpp

--- pass_obj_func_arg.cpp
+++ pass_obj_func_arg.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
 
-class complex
-{
+class complex{
     int a, b;
 
 public:
-    void setData(int v1, int v2)
-    {
+    void setData(int v1, int v2){
         a = v1;
         b = v2;
     }
