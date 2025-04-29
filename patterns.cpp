c1a5329fa5a0268fe76352294cac37eedcbb5c64
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-04-29 10:50:58 +0530

    Create patterns.cpp

---
+++ patterns.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << "*\t";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
