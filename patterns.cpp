af4ced654f6b54c1e70ec4827de8302deb791449
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-04-30 11:48:10 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -85,6 +85,58 @@ int main()
         }
         cout<<endl;
     }
+
+
+
+	//triangle pattern with numbers for n = 4
+    //required output
+  /*1
+    22
+    333
+    4444 */
+   
+    int n = 4;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            cout<<i+1;
+        }
+        cout<<endl;
+    }
+	
+	
+	
+	//triangle patter(2) n = 4
+    /*required pattern
+    1
+    1 2
+    1 2 3
+    1 2 3 4*/
+
+    int n = 4;
+    for(int i = 0; i < n; i++){
+        for(int j = 1; j <= i + 1; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+	
+	
+	
+	// reverse triangle patter(2) n = 4
+    /*required pattern
+    1
+    2 1
+    3 2 1
+    4 3 2 1*/
+
+    int n = 4;
+
+    for (int i = 0; i < n; i++){
+        for(int j = i + 1; j > 0; j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
     
     return 0;
 }
