8cd9735f9fd322e72ca49fe991167ffd061d8ecf
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-04-30 11:48:31 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -137,6 +137,47 @@ int main()
         }
         cout<<endl;
     }
+
+
+	///Floyd's Triangle Pattern
+    /*n = 4
+    1
+    2 3
+    4 5 6
+    7 8 9 10*/
+
+    int n = 4;
+    int num = 1;
+
+    for(int i = 0;i < n; i++){
+        for(int j = 1; j <= i+1; j++){
+            cout<<num << " ";
+            num++;
+        }
+        cout<<endl;
+    }
+	
+	
+	
+	///Inverted Triangle Pattern
+    /*n = 4
+    1 1 1 1
+      2 2 2
+        3 3
+          4*/
+    
+    int n = 4;
+    for(int i = 0; i < n; i++){
+		//spaces
+        for(int j = 0; j < i; j++){
+            cout<<" ";
+        }
+		//nums
+        for(int j = 0; j < n-i; j++){
+            cout<<(i+1);
+        }
+        cout<<endl;
+    }
     
     return 0;
 }
